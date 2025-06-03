5623fbdb9c4c6b9c347fbb00160d0b892811c41a
Ana Beatriz <anabeatrizkraener@gmail.com>
2025-06-03 13:26:50 -0300

    Alterações no footer

---
+++ view/footer.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="../fonts/fonts.css">
+</head>
+<body class="jetBrains flex justify-center mt-20 gap-10">
+
+<div  style="backgroud-color: #65fd00">
+</div>
+
+    <div class="px-12 py-4 rounded-3xl hover:scale-105 hover:transition duration-600 group" style="background-color: #1f1f1f; box-shadow: -10px 10px 0px black">
+        <img src="../imgs/ana.jpg" alt="foto" class="w-32 border border-2" style="border-radius: 50px; border-color: #65fd00">
+
+        <div class="jetBrainsBold text-xl text-white text-center mt-2 hover:transition duration-700 group-hover:text-[color:#65fd00]">
+            <h2>Ana</h2>
+            <a href="https://github.com/Anaaf7">Anaaf7</a>
+        </div>
+    </div>
+
+    <div class="px-12 py-4 rounded-3xl hover:scale-105 hover:transition duration-600 group" style="background-color: #1f1f1f; box-shadow: -10px 10px 0px black">
+        <img src="../imgs/isadora.jpg" alt="foto" class="w-32 border border-2" style="border-radius: 50px; border-color: #cc15e2">
+
+        <div class="jetBrainsBold text-xl text-white text-center mt-2 hover:transition duration-700 group-hover:text-[color:#cc15e2]">
+            <h2>Isadora</h2>
+            <a href="https://github.com/IsadoraLSch">IsadoraLSch</a>
+        </div>
+    </div>
+
+    <div class="px-12 py-4 rounded-3xl hover:scale-105 hover:transition duration-600 group" style="background-color: #1f1f1f; box-shadow: -10px 10px 0px black">
+        <img src="../imgs/bianca.jpg" alt="foto" class="w-32 border border-2" style="border-radius: 50px; border-color: #feac01">
+
+        <div class="jetBrainsBold text-xl text-white text-center mt-2 hover:transition duration-700 group-hover:text-[color:#feac01]">
+            <h2>Bianca</h2>
+            <a href="https://github.com/Biahjwff">Biahjwff</a>
+        </div>
+    </div>
+
+    <div class="px-12 py-4 rounded-3xl hover:scale-105 hover:transition duration-600 group" style="background-color: #1f1f1f; box-shadow: -10px 10px 0px black">
+        <img src="../imgs/alice.jpg" alt="foto" class="w-32 border border-2" style="border-radius: 50px; border-color: #65fd00">
+
+        <div class="jetBrainsBold text-xl text-white text-center mt-2 hover:transition duration-700 group-hover:text-[color:#65fd00]">
+            <h2>Alice</h2>
+            <a href="https://github.com/Alice130109">Alice130109</a>
+        </div>
+    </div>
+</body>
+</html>
